e84994049f32673b066653dedb6db3a6f50303e5
Jobjörn Folkesson <jobjorn@jobjorn.se>
2023-11-27 21:51:09 +0100

    toggle för statistikperioder!

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -5,7 +5,7 @@ import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import { Footer } from 'components/Footer';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { Typography } from '@mui/material';
+import { ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { faviconString } from 'services/helpers';
 import Head from 'next/head';
 import { Loading } from 'components/Loading';
@@ -73,10 +73,12 @@ ChartJS.register(
 export const StatsPage: NextPage<{}> = () => {
   let [stats, setStats] = useState<StatsData>();
   let [statsData, setStatsData] = useState<any>();
+  let [period, setPeriod] = useState<string>('daily');
 
   const { user, isLoading } = useUser(); // härifrån finns också error att ta ut
 
   const options = {
+    animation: false,
     responsive: true,
     interaction: {
       mode: 'index',
@@ -114,27 +116,126 @@ export const StatsPage: NextPage<{}> = () => {
     fetchStats();
   }, []);
 
+  function groupDataByWeek(data: StatsData): StatsData {
+    const weeklyData: StatsData = [];
+    let currentWeek: {
+      day: string;
+      unique_users_with_turns: number;
+      total_moves: number;
+    } | null = null;
+
+    for (const stat of data) {
+      const statDate = new Date(stat.day);
+      const weekStartDate = new Date(
+        statDate.getFullYear(),
+        statDate.getMonth(),
+        statDate.getDate() - statDate.getDay()
+      );
+
+      if (
+        currentWeek === null ||
+        weekStartDate.toISOString() !== currentWeek.day
+      ) {
+        // New week
+        if (currentWeek !== null) {
+          weeklyData.push(currentWeek);
+        }
+
+        currentWeek = {
+          day: weekStartDate.toISOString(),
+          unique_users_with_turns: stat.unique_users_with_turns,
+          total_moves: stat.total_moves
+        };
+      } else {
+        // Accumulate data for the current week
+        currentWeek.unique_users_with_turns += stat.unique_users_with_turns;
+        currentWeek.total_moves += stat.total_moves;
+      }
+    }
+
+    // Add the last week
+    if (currentWeek !== null) {
+      weeklyData.push(currentWeek);
+    }
+
+    return weeklyData;
+  }
+
+  function groupDataByMonth(data: StatsData): StatsData {
+    const monthlyData: StatsData = [];
+    let currentMonth: {
+      day: string;
+      unique_users_with_turns: number;
+      total_moves: number;
+    } | null = null;
+
+    for (const stat of data) {
+      const statDate = new Date(stat.day);
+      const monthStartDate = new Date(
+        statDate.getFullYear(),
+        statDate.getMonth(),
+        1
+      );
+
+      if (
+        currentMonth === null ||
+        monthStartDate.toISOString() !== currentMonth.day
+      ) {
+        // New month
+        if (currentMonth !== null) {
+          monthlyData.push(currentMonth);
+        }
+
+        currentMonth = {
+          day: monthStartDate.toISOString(),
+          unique_users_with_turns: stat.unique_users_with_turns,
+          total_moves: stat.total_moves
+        };
+      } else {
+        // Accumulate data for the current month
+        currentMonth.unique_users_with_turns += stat.unique_users_with_turns;
+        currentMonth.total_moves += stat.total_moves;
+      }
+    }
+
+    // Add the last month
+    if (currentMonth !== null) {
+      monthlyData.push(currentMonth);
+    }
+
+    return monthlyData;
+  }
+
   useEffect(() => {
     if (stats) {
+      let chartStats = stats;
+      if (period === 'weekly') {
+        chartStats = groupDataByWeek(stats);
+      }
+      if (period === 'monthly') {
+        chartStats = groupDataByMonth(stats);
+      }
       let newStatsData = {
-        labels: stats.map((stat) => new Date(stat.day).toLocaleDateString()),
+        labels: chartStats.map((stat) =>
+          new Date(stat.day).toLocaleDateString()
+        ),
         datasets: [
           {
             label: 'Antal användare som gjort minst ett drag',
-            data: stats.map((stat) => stat.unique_users_with_turns),
+            data: chartStats.map((stat) => stat.unique_users_with_turns),
             borderColor: 'rgb(255, 99, 132)',
             backgroundColor: 'rgb(255, 99, 132)',
             yAxisID: 'y',
-            tension: 0.5,
+            tension: 0.2,
             pointStyle: false
           },
           {
             label: 'Antal drag',
-            data: stats.map((stat) => stat.total_moves),
+            data: chartStats.map((stat) => stat.total_moves),
             borderColor: 'rgb(53, 162, 235)',
             backgroundColor: 'rgb(53, 162, 235)',
             yAxisID: 'y1',
-            tension: 0.5,
+            tension: 0.2,
             pointStyle: false
           }
         ]
@@ -142,7 +243,16 @@ export const StatsPage: NextPage<{}> = () => {
 
       setStatsData(newStatsData);
     }
-  }, [stats]);
+  }, [stats, period]);
+
+  const handlePeriod = (
+    event: React.MouseEvent<HTMLElement>,
+    newPeriod: string
+  ) => {
+    if (newPeriod !== null) {
+      setPeriod(newPeriod);
+    }
+  };
 
   if (isLoading || !user || !stats || !statsData) {
     return <Loading />;
@@ -168,6 +278,23 @@ export const StatsPage: NextPage<{}> = () => {
         <Typography variant="h4" sx={{}}>
           Statistik
         </Typography>
+        <ToggleButtonGroup
+          value={period}
+          exclusive
+          onChange={handlePeriod}
+          aria-label="chart periods"
+          sx={{ my: 1 }}
+        >
+          <ToggleButton value="daily" aria-label="left aligned">
+            Per dag
+          </ToggleButton>
+          <ToggleButton value="weekly" aria-label="centered">
+            Per vecka
+          </ToggleButton>
+          <ToggleButton value="monthly" aria-label="right aligned">
+            Per månad
+          </ToggleButton>
+        </ToggleButtonGroup>
         <Line options={options} data={statsData} />
       </Container>
       <Footer />
